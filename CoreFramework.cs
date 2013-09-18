d4ca0e63e1f09d3f48d1dc44af6766807ba520e4
jc <jiowchern@yahoo.com.tw>
2013-09-18 18:22:30 +0800

    initial

---
+++ CoreFramework.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoreFramework : MonoBehaviour {
+
+    Regulus.Project.Crystal.IUser _User;
+	// Use this for initialization
+	void Start () 
+    {
+        _User = new Regulus.Project.Crystal.Standalone.User();
+        _User.VerifyProvider.Supply += _GetVerify;
+
+	}
+
+    void _GetVerify(Regulus.Project.Crystal.IVerify obj)
+    {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	
+	}
+}
