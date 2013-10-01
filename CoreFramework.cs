4bc8f7cd220d3ae910710707647203dae88dbf49
jc <jiowchern@yahoo.com.tw>
2013-10-01 14:02:32 +0800

    add core framework

--- CoreFramework.cs
+++ CoreFramework.cs
@@ -1,25 +1,73 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CoreFramework : MonoBehaviour {
 
-    Regulus.Project.Crystal.IUser _User;
-	// Use this for initialization
+public class CoreFramework : MonoBehaviour, Regulus.Utility.Console.IViewer, Regulus.Utility.Console.IInput
+{
+    Regulus.Game.IFramework _Application;
+    private string _Input = "";
+    
 	void Start () 
     {
-        _User = new Regulus.Project.Crystal.Standalone.User();
-        _User.VerifyProvider.Supply += _GetVerify;
-
+        _Application = new Regulus.Project.Crystal.Application(this, this, new Regulus.Game.IFramework[0]);
+        _Application.Launch();        
 	}
 
-    void _GetVerify(Regulus.Project.Crystal.IVerify obj)
+    System.Collections.Generic.Queue<string> _List = new System.Collections.Generic.Queue<string>();
+	// Update is called once per frame
+    bool _ShowConsole = false;
+	void Update () 
+    {
+        _Application.Update();
+
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            _ShowConsole = !_ShowConsole;
+        }
+	}
+
+    void OnGUI()
+    {
+        if (_ShowConsole)
+        {
+            GUILayout.BeginVertical();
+
+            foreach (var message in _List)
+            {
+                GUILayout.Label(message);
+            }
+            GUILayout.EndVertical();
+            GUILayout.BeginHorizontal();
+            _Input = GUILayout.TextField(_Input);
+            if (GUILayout.Button("輸入"))
+            {
+                _OutputEvent(_Input.Split(new char[] { ' ' }, System.StringSplitOptions.RemoveEmptyEntries));
+                _Input = "";
+            }
+            GUILayout.EndHorizontal();
+        }
+        
+    }
+
+    void Regulus.Utility.Console.IViewer.Write(string message)
     {
         
     }
-	
-	// Update is called once per frame
-	void Update () 
+
+    void Regulus.Utility.Console.IViewer.WriteLine(string message)
     {
-	
-	}
+        _List.Enqueue(message);
+        if (_List.Count > 25)
+        {
+            _List.Dequeue();
+        }
+    }
+
+    event Regulus.Utility.Console.OnOutput _OutputEvent;
+    event Regulus.Utility.Console.OnOutput Regulus.Utility.Console.IInput.OutputEvent
+    {
+        add { _OutputEvent += value; }
+        remove { _OutputEvent -= value; }
+    }
 }
+
