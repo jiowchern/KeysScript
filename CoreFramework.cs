783c5671a6fde8a9b269f42c87571fe3ec695d99
jc <jiowchern@yahoo.com.tw>
2013-10-25 18:38:53 +0800

    add user

--- CoreFramework.cs
+++ CoreFramework.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 
 
+
 public class CoreFramework : MonoBehaviour, Regulus.Utility.Console.IViewer, Regulus.Utility.Console.IInput
 {
     Regulus.Project.Crystal.Application _Application;
@@ -15,33 +16,49 @@ public class CoreFramework : MonoBehaviour, Regulus.Utility.Console.IViewer, Reg
     public GameObject BattleStage;
 
     GameObject _CurrentStage;
-
+    System.Collections.Generic.List<Regulus.Project.Crystal.IUser> _Users ;
     Regulus.Project.Crystal.IUser _CurrentUser;
+    public CoreFramework()
+    {
+        _Users = new System.Collections.Generic.List<Regulus.Project.Crystal.IUser>();
+    }
+
+    ~CoreFramework()
+    {
+
+    }
+    
+
 	void Start () 
     {
         var app = new Regulus.Project.Crystal.Application(this, this);        
         _Application = app;        
-
         
         _Application.UserSpawnEvent += _OnSpawnUser;
-        (_Application as Regulus.Game.IFramework).Launch();
-        
+        _Application.UserUnspawnEvent += _OnUnspawnUser;
+        (_Application as Regulus.Game.IFramework).Launch();        
 	}
-
-    System.Collections.Generic.List<Regulus.Project.Crystal.IUser> _Users = new System.Collections.Generic.List<Regulus.Project.Crystal.IUser>();
+    
     private void _OnSpawnUser(Regulus.Project.Crystal.IUser user)
     {
         _Users.Add(user);
 
-        _Application.Command.Register("look"  + user.GetHashCode() ,  () =>
+        _Application.Command.Register("Lookup"  + user.GetHashCode() ,  () =>
         {
-            if (_CurrentUser != null)
-            {
-                _CurrentUser.StatusProvider.Supply -= _OnGameStatus;
-            }
-            _CurrentUser = user;
-            _CurrentUser.StatusProvider.Supply += _OnGameStatus;
+            _LookupUser(user);
         });
+    }
+
+    void _OnUnspawnUser(Regulus.Project.Crystal.IUser user)
+    {
+        _Application.Command.Unregister("Lookup" + user.GetHashCode());
+        _Users.Remove(user);
+    }
+
+    private void _LookupUser(Regulus.Project.Crystal.IUser user)
+    {
+        user.StatusProvider.Supply += _OnGameStatus;
+        
     }
 
     void _OnGameStatus(Regulus.Project.Crystal.IUserStatus obj)
@@ -49,35 +66,38 @@ public class CoreFramework : MonoBehaviour, Regulus.Utility.Console.IViewer, Reg
         obj.StatusEvent += _OnChangeStatus;
     }
 
+    
+
     void _OnChangeStatus(Regulus.Project.Crystal.UserStatus obj)
     {
         GameObject[] stages = new GameObject[]{LogoStage , VerifyStage , ParkingStage , AdventureStage , BattleStage} ;
-        var stage = GameObject.Instantiate(stages[(int)obj]) as GameObject;
-        var cs = stage.GetComponent<CreateSystem>();
-        cs.SetUser(_CurrentUser);
+        var stage = GameObject.Instantiate(stages[(int)obj]) as GameObject;        
         _ChangeStage(stage);
         Debug.Log("切換場景" + obj.ToString());
     }
 
-
-
     void _ChangeStage(GameObject new_stage )
     {        
         if (_CurrentStage != null)
-        {            
-            GameObject.Destroy(_CurrentStage);
-            
+        {
+            _RestroyStage();
         }
-        _CurrentStage = new_stage;
+        _CreateStage(new_stage);
     }
-    private void _ShowLogoStage()
+
+    private void _CreateStage(GameObject new_stage)
     {
-        var stage = GameObject.Instantiate(LogoStage) as GameObject;
-        var createSystem = stage.GetComponent<CreateSystem>();
-        
-        
-        _ChangeStage(stage);
+        _CurrentStage = new_stage;
+        var user = _CurrentStage.GetComponent<User>();
+        user.Initital(_CurrentUser);
     }
+
+    private void _RestroyStage()
+    {
+        var user = _CurrentStage.GetComponent<User>();
+        user.Release(_CurrentUser);
+        GameObject.Destroy(_CurrentStage);
+    }    
 
     System.Collections.Generic.Queue<string> _List = new System.Collections.Generic.Queue<string>();
 	// Update is called once per frame
@@ -86,7 +106,7 @@ public class CoreFramework : MonoBehaviour, Regulus.Utility.Console.IViewer, Reg
     {
         (_Application as Regulus.Game.IFramework).Update();
 
-        if (Input.GetKeyUp(KeyCode.A))
+        if (Input.GetKeyUp(KeyCode.BackQuote))
         {
             _ShowConsole = !_ShowConsole;
         }
---
+++ User.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+
+class User : MonoBehaviour
+{
+    public delegate void OnSpawnUser(Regulus.Project.Crystal.IUser user);
+    public event OnSpawnUser SpawnUserEvent;
+
+    public delegate void OnUnspawnUser(Regulus.Project.Crystal.IUser user);
+    public event OnUnspawnUser UnspawnUserEvent;
+
+    internal void Release(Regulus.Project.Crystal.IUser user)
+    {
+        if (SpawnUserEvent != null)
+            SpawnUserEvent(user);
+    }
+
+    internal void Initital(Regulus.Project.Crystal.IUser user)
+    {
+        if (UnspawnUserEvent != null)
+            UnspawnUserEvent(user);
+    }
+}
+
